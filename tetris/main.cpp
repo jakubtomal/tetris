cbf3951ebae3136d38e493a5a46ee0710a228658
informatykastosowana <jakub.tomal@smcebi.edu.pl>
2019-06-06 08:13:13 +0200

    ogolny core gry, mechanika , score

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <string>
 
-using namespace sf;
+using namespace sf ;
 
 const int height = 20;
 const int width = 10;
@@ -38,21 +39,37 @@ int main()
 {
 	srand(time(0));
 
-	RenderWindow window(VideoMode(480, 800), "*** TETRIS ***");
 
-	Sprite colors[6];
+	//Graphic
+	RenderWindow window(VideoMode(480, 700), "*** TETRIS ***");
 
-	Texture t;
+	Font font;
+	font.loadFromFile("assets/RacingNumbers.ttf");
+	Text textPoints;
+	textPoints.setFont(font);
+	textPoints.setCharacterSize(50);
+	textPoints.setFillColor(Color::Blue);
+	textPoints.setOutlineColor(Color::Red);
+	textPoints.setOutlineThickness(2);
+	textPoints.setPosition(30, 635);
+	Texture t , t2;
 	t.loadFromFile("assets/blocks.png");
-	Sprite s(t);
+	t2.loadFromFile("assets/background.png");
+	Sprite s(t),background(t2);
 	s.setTextureRect(IntRect(0, 0, 32, 32));
 
 
+
+	//Game Config
 	int dx = 0;
+	int points = 0;
 	bool rotate = false;
+	bool running = true;
 	int numOfColor = 1;
 	float timer = 0;
-	float delay = 0.3;
+	float startDelay = 0.3;
+	float currentDelay = startDelay;
+	float delay = startDelay;
 
 	int n = rand() % 7;
 	for (int i = 0; i < 4; i++)
@@ -67,6 +84,8 @@ int main()
 
 	while (window.isOpen())
 	{
+
+		textPoints.setString("Score "+std::to_string(points));
 		
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
@@ -83,65 +102,99 @@ int main()
 				if (event.key.code == Keyboard::Up) rotate = true;
 				else if (event.key.code == Keyboard::Left) dx = -1;
 				else if (event.key.code == Keyboard::Right) dx = 1;
+				else if (event.key.code == Keyboard::R)
+				{
+					for (int i = 0; i < height; i++)
+					{
+						for (int j = 0; j < width; j++)
+						{
+							
+							map[i][j] = 0;
+							for (int i = 0; i < 4; i++)
+							{
+								a[i].x = tetrominoes[n][i] % 2;
+								a[i].y = tetrominoes[n][i] / 2;
+
+							}
+							points = 0;
+							currentDelay = startDelay;
+							running = true;
+						}
+					};
+				}
 			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down)) delay = 0.05;
 
-		//move
+		//check for lose
 
-		for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
-		if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+		if (!check() && (a[0].y  = tetrominoes[n][0] / 2)) running = false;
 
-		//rotate
 
-		if (rotate)
+		if (running)
 		{
+			//move
 
-			Point point = a[1]; //center of rotation
-			for (int i = 0; i < 4; i++)
+			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].x += dx; }
+			if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+			//rotate
+
+			if (rotate)
 			{
-				int x = a[i].y - point.y;
-				int y = a[i].x - point.x;
-				a[i].x = point.x - x;
-				a[i].y = point.y + y;
-			}
 
-			if(!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
-
-		}
-
-		if (timer > delay)
-		{
-			for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
-
-			if (!check()) {
-				for (int i = 0; i < 4; i++) map[b[i].y][b[i].x] = numOfColor;
-
-				numOfColor = 1 + rand() % 7;
-				int n = rand() % 7;
+				Point point = a[1]; //center of rotation
 				for (int i = 0; i < 4; i++)
 				{
-					a[i].x = tetrominoes[n][i] % 2;
-					a[i].y = tetrominoes[n][i] / 2;
+					int x = a[i].y - point.y;
+					int y = a[i].x - point.x;
+					a[i].x = point.x - x;
+					a[i].y = point.y + y;
+				}
 
+				if (!check()) for (int i = 0; i < 4; i++) a[i] = b[i];
+
+			}
+
+			if (timer > delay)
+			{
+				for (int i = 0; i < 4; i++) { b[i] = a[i]; a[i].y += 1; }
+
+				if (!check()) {
+					for (int i = 0; i < 4; i++) map[b[i].y][b[i].x] = numOfColor;
+
+					numOfColor = 1 + rand() % 6;
+					int n = rand() % 7;
+					for (int i = 0; i < 4; i++)
+					{
+						a[i].x = tetrominoes[n][i] % 2;
+						a[i].y = tetrominoes[n][i] / 2;
+
+					}
+				}
+
+				timer = 0;
+			}
+
+			// check lines
+
+			int k = height-1 ;
+			for (int i = height-1 ; i > 0; i--)
+			{
+				int count = 0;
+				for (int j = 0; j < width; j++)
+				{
+					if (map[i][j]) count++;
+					map[k][j] = map[i][j];
+				}
+				if (count < width) k--;
+				else
+				{
+					points++;
+					if (currentDelay <= 0.1) currentDelay -= 0.01;
+					
 				}
 			}
-
-			timer = 0;
-		}
-
-		// check lines
-
-		int k = height - 1;
-		for (int i = height - 1; i > 0; i--)
-		{
-			int count = 0;
-			for (int j = 0; j < width; j++)
-			{
-				if (map[i][j]) count++;
-				map[k][j] = map[i][j];
-			}
-			if (count < width) k--;
 		}
 
 
@@ -151,7 +204,8 @@ int main()
 
 		//DRAW
 		window.clear(Color::White);
-
+		window.draw(background);
+		window.draw(textPoints);
 		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
@@ -179,7 +233,7 @@ int main()
 		//restart move and rotate
 		dx = 0;
 		rotate = 0;
-		delay = 0.3;
+		delay = currentDelay;
 	}
 
 	return 0;
